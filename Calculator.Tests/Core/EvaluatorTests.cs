4b16110461a2090f0f36622ac73d0cb36fe13d6b
Mikuláš Čajánek <mcajanek@email.cz>
2025-11-30 16:30:21 +0100

    #IMROVEMENT - created base test structure

---
+++ Calculator.Tests/Core/EvaluatorTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests.Core
+{
+    internal class EvaluatorTests
+    {
+    }
+}
